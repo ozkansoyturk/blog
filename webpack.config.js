0c864a2c78ae85ba8ff2019821e07ad6360c4fa7
ozkansoyturk <soyturkozkan@gmail.com>
2023-03-17 17:38:59 +0100

    setup project 2

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: path.join(__dirname, "src/index.js"),
+    form: path.join(__dirname, "src/form/form.js"),
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        exclude: /(node_modules)/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.scss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: path.join(__dirname, "./src/index.html"),
+      chunks: ["main"],
+    }),
+    new HtmlWebpackPlugin({
+      filename: "form.html",
+      template: path.join(__dirname, "./src/form/form.html"),
+      chunks: ["form"],
+    }),
+  ],
+  stats: "minimal",
+  devtool: "source-map",
+  mode: "development",
+  devServer: {
+    open: false,
+    static: path.resolve(__dirname, "./dist"),
+    watchFiles: ["./src/**"],
+    port: 4000,
+    hot: true,
+  },
+};

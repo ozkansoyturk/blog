e7fbc697db76cb6be75a9b33fbbaf6d0bab9a842
ozkansoyturk <soyturkozkan@gmail.com>
2023-03-20 23:44:39 +0100

    ok

--- src/form/form.js
+++ src/form/form.js
@@ -1,2 +1,14 @@
 import "../assets/styles/styles.scss";
 import "./form.scss";
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+
+  for (const pair of formData) {
+    // console.log(pair[0]);
+  }
+});

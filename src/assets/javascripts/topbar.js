5f9c22ac1515a3bdbd8ee5ef3fff6cf328e24e68
ozkansoyturk <soyturkozkan@gmail.com>
2023-03-22 01:15:14 +0100

    Mise en place du JavaScript pour le formulaire

--- src/assets/javascripts/topbar.js
+++ src/assets/javascripts/topbar.js
@@ -1,1 +1,1 @@
-console.log("topbar");
+// console.log("topbar");
--- src/form/form.js
+++ src/form/form.js
@@ -2,13 +2,34 @@ import "../assets/styles/styles.scss";
 import "./form.scss";
 
 const form = document.querySelector("form");
+const errorElement = document.querySelector("#errors");
+let errors = [];
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const formData = new FormData(form);
-
-  for (const pair of formData) {
-    // console.log(pair[0]);
+  const article = Object.fromEntries(formData.entries());
+  if (formIsValide(article)) {
+    const json = JSON.stringify(article);
+    // fetch
   }
 });
+
+const formIsValide = (article) => {
+  if (!article.author || !article.category || !article.content) {
+    errors.push("Vous devez renseigner tous les champs");
+  } else {
+    errors = [];
+  }
+
+  if (errors.length) {
+    let errorHtml = "";
+    errors.forEach((e) => {
+      errorHtml += `<li>${e}</li>`;
+    });
+    errorElement.innerHTML = errorHtml;
+  } else {
+    errorElement.innerHTML = "";
+  }
+};

2799a3ef5229eef261ae0651f2cb0f249b053267
ozkansoyturk <soyturkozkan@gmail.com>
2023-03-24 18:29:52 +0100

    ajout date, boutton annulation et redirection

--- src/form/form.js
+++ src/form/form.js
@@ -3,7 +3,12 @@ import "./form.scss";
 
 const form = document.querySelector("form");
 const errorElement = document.querySelector("#errors");
+const buttonCancel = document.querySelector(".btn-secondary");
 let errors = [];
+
+buttonCancel.addEventListener("click", (event) => {
+  location.assign("/index.html");
+});
 
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
@@ -13,9 +18,8 @@ form.addEventListener("submit", async (event) => {
   console.log(article);
 
   if (formIsValide(article)) {
-    const json = JSON.stringify(article);
-
     try {
+      const json = JSON.stringify(article);
       const response = await fetch("https://restapi.fr/api/ok", {
         method: "POST",
         body: json,
@@ -23,10 +27,11 @@ form.addEventListener("submit", async (event) => {
           "Content-Type": "application/json",
         },
       });
-      const body = await response.json();
-      console.log(body);
       errorElement.innerHTML = `<li>Article sauvegarder</li>`;
       errorElement.style.color = "#2ecc71";
+      if (response.status < 299) {
+        location.assign("/index.html");
+      }
     } catch (err) {
       console.log(err);
     }
@@ -34,6 +39,7 @@ form.addEventListener("submit", async (event) => {
 });
 
 const formIsValide = (article) => {
+  errors = [];
   if (
     !article.author ||
     !article.category ||
